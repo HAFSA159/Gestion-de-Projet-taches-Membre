5ee8cd95694a2236bf0e936b57b4a63e05b46d2b
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-10-04 17:26:30 +0100

    all d

--- src/main/java/com/example/taskmanager/model/Role.java
+++
@@ -1,7 +0,0 @@
-package com.example.taskmanager.model;
-
-public enum Role {
-    PROJECT_MANAGER,
-    DEVELOPER,
-    DESIGNER
-}
--- src/main/java/com/example/taskmanager/service/TaskService.java
+++
@@ -1,5 +0,0 @@
-package com.example.taskmanager.service;
-
-public class TaskService {
-
-}
